52838b366a27be0356e24eeaeaf7defa98bca471
islamibragim1 <ibragimovislam58@gmail.com>
2023-03-30 18:59:29 -0700

    add reverse integer

--- CS380/src/packagetest/CS380_Test.java
+++ CS380/src/packagetest/CS380_Test.java
@@ -3,6 +3,17 @@ package packagetest;
 public class CS380_Test {
 	public static void main(String[] args) {
 		System.out.println("The First Lab Cs 380");
+		System.out.println(reverseInteger(12345));
+	}
+
+	public static int reverseInteger(int n) {
+		int reversed = 0;
+		while (n != 0) {
+			int digit = n % 10;
+			reversed = reversed * 10 + digit;
+			n /= 10;
+		}
+		return reversed;
 
 	}
 }
